3af125c4465dcf80a264aa2870183449bdcd9ec1
swarnacoder <135952525+swarnacoder@users.noreply.github.com>
2023-12-22 13:19:36 +0530

    added database models

---
+++ server/models/jobModel.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema({
+    companyName:{
+        type:String,
+        required:true
+    },
+    companyLogoURL:{
+        type:String,
+        required:true
+    },
+    jobPosition:{
+        type:String,
+        required:true
+    },
+    monthlySalary:{
+        type:String,
+        required:true
+    },
+    jobType:{
+        type:String,
+        enum:["Part-time","Full-time","Internship"],
+        required:true
+    },
+    remote:{
+        type:String,
+        enum:["Office","Remote"],
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    jobDescription:{
+        type:String,
+        required:true
+    },
+    aboutCompany:{
+        type:String,
+        required:true
+    },
+    skillsRequired:{
+        type:[String],
+        required:true
+    },
+    information:{
+        type:String,
+        required:true
+    },
+    createdAt: {
+        type: Date,
+        default: () => Date.now()
+    },
+})
+
+const Job = mongoose.model("Job",jobSchema);
+
+module.exports = Job;
