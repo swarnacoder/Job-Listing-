b623cf51612f6c87c80f38f688cf720d00659289
swarnacoder <135952525+swarnacoder@users.noreply.github.com>
2023-12-23 08:46:34 +0530

    added error handler middleware

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -1,10 +1,9 @@
-// errorMiddleware.js
 const errorMiddleware = (err, req, res, next) => {
     const status = err.status || 500;
     const message = err.message || 'Something went wrong! Please try after some time.';
     const extraDetails = err.extraDetails || 'Error from the Backend';
   
-    // Log the error (optional)
+    // Log the error 
     console.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${extraDetails}`);
   
     // Send the error response
